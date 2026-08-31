143a65aaaf64b50cd87b5f613a004768ecc96a37
seelfidha <seelfidha@gmail.com>
2026-08-31 09:28:08 -0400

    fix delay reading data by the trainer

--- repository/processed_id_repository.py
+++ repository/processed_id_repository.py
@@ -1,31 +1,29 @@
-from repository.repository_config import get_connection
+from repository.repository_config import get_connection_with_retries
 
 
 def is_not_processed(item_id):
     try:
-        conn = get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute('SELECT 1 FROM processed_ids where id =%s', (item_id,))
-            return cursor.fetchone() is None
-    except Exception as e :
+        with get_connection_with_retries(10, 3) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute('SELECT 1 FROM processed_ids where id =%s', (item_id,))
+                return cursor.fetchone() is None
+    except Exception:
         print(f"error checking id already processed: {item_id}")
-        conn.rollback()
-        return False
+        raise
 
 def mark_as_processed(item_id):
     try:
-        conn = get_connection()
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """
-                INSERT INTO processed_ids (id)
-                VALUES (%s) ON CONFLICT (id) DO NOTHING
-                """,
-                (item_id,)
-            )
-            conn.commit()
-            return True
-    except Exception as e :
-        print(f"error saving id: %s", item_id)
-        conn.rollback()
-        return False
+        with get_connection_with_retries(10, 3) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO processed_ids (id)
+                    VALUES (%s) ON CONFLICT (id) DO NOTHING
+                    """,
+                    (item_id,)
+                )
+                conn.commit()
+                return True
+    except Exception:
+        print(f"error saving id: {item_id}")
+        raise
--- repository/repository_config.py
+++ repository/repository_config.py
@@ -4,14 +4,15 @@ import psycopg
 
 def get_connection_with_retries(max_retries=10, delay_seconds=3):
     last_error = None
-    for attempt in range(1, max_retries +1):
+    for attempt in range(1, max_retries + 1):
         try:
             return get_connection()
-        except psycopg.OperationalError as error :
+        except psycopg.OperationalError as error:
             last_error = error
             print(f"Database connection failed on attempt {attempt} error: {last_error}", flush=True)
             if attempt < max_retries:
                 time.sleep(delay_seconds)
+    raise last_error
 
 def get_connection():
     return psycopg.connect(
