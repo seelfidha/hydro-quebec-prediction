818e5cefd74e10ca3c13049b4c42c94779aef7f4
seelfidha <seelfidha@gmail.com>
2026-09-17 00:18:26 -0400

    fix buttons behaviour

--- utils/params_config.py
+++ utils/params_config.py
@@ -1,18 +1,18 @@
 
-db_host = "dpg-daec7eht0dsc739l7i50-a"#postgres#localhost
+db_host = "localhost"
 db_port = "5432"
-db_name = "interruption_db"
+db_name = "test_db"
 db_user = "root"
-db_pw = "4WvlYwM7SbMfxp27cToQHC62tw1giS8a"#root
+db_pw = "root"
 
-ml_flow_host = "http://mlflow"
+ml_flow_host = "http://localhost"
 ml_flow_port = "5000"
 ml_flow_url = ml_flow_host+":"+ml_flow_port
 
 h2o_host = "127.0.0.1"
 h2o_port = "5000"
 
-minio_host = "minio"
+minio_host = "localhost"
 minio_port = "9000"
 minio_url = minio_host+":"+minio_port
 minio_access_key = "admin"
